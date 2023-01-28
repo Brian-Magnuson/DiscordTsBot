026dcc29e869f368e7cd7a396380738d5936e1c9
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2023-01-28 15:49:06 -0500

    Changed interactionCreate.ts to check button's customId

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -1,12 +1,14 @@
 import {
 	Events,
-	BaseInteraction
+	BaseInteraction,
+	ButtonInteraction
 } from "discord.js";
 import ExtendedClient from "types/ExtendedClient";
 
 module.exports = {
 	name: Events.InteractionCreate,
 	async execute(interaction: BaseInteraction) {
+		// CHAT INPUT COMMANDS
 		if (interaction.isChatInputCommand()) {
 			const command = (interaction.client as ExtendedClient).commands.get(interaction.commandName);
 
@@ -21,8 +23,16 @@ module.exports = {
 				console.error(`Error executing ${interaction.commandName}`);
 				console.error(error);
 			}
+			// BUTTON COMMANDS
 		} else if (interaction.isButton()) {
-			interaction.reply(`${interaction.user.username} clicked the button!`);
+			const customId = (interaction as ButtonInteraction).customId;
+
+			// Branch on button's customId
+			if (customId == 'primary') {
+				interaction.reply(`${interaction.user.username} clicked the button.`)
+			} else {
+				console.log(`[WARNING]: No button interaction handler exists for ${customId}`)
+			}
 		} else {
 			return;
 		}
