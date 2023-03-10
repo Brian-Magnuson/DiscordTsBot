e37530a6f1f5e06f22e1fabe3bbf40f590b0f800
Brian-Magnuson <92892499+Brian-Magnuson@users.noreply.github.com>
2023-03-10 16:04:17 -0500

    Revert "Create individual functions for the event handlers"
    This reverts commit d4ae287db5d2860834661c990999ddda564d1cdd.

--- events/button/primary.ts
+++
@@ -1,9 +0,0 @@
-import { ButtonInteraction } from "discord.js";
-
-export async function primary(interaction: ButtonInteraction, customId: string): Promise<boolean> {
-  const match = customId == 'primary';
-  if (match) {
-    interaction.reply(`${interaction.user.username} clicked the button.`);
-  }
-  return match;
-}
--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -44,7 +44,7 @@ module.exports = {
 			// Branch on menu's customId
 			if (customId == 'select') {
 				const selected = interaction.values[0];
-				interaction.reply(`You selected ${selected}!`)
+				await interaction.reply(`You selected ${selected}!`)
 			} else {
 				console.log(`[WARNING]: No string select menu interaction handler exists for ${customId}`)
 			}
